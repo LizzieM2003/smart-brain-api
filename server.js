455287f2fe18fbe6ed8b33afecef3f8508b1bfef
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2018-03-11 21:23:55 +0000

    change start script

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json('Home page');
+  res.send('yippee - it works!');
 });
 
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
